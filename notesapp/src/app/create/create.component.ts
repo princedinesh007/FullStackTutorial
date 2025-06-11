9287d09e7e6263b01baeabbd0f1f8bf7f54ea419
Dineshkumar Arunachalam <dineshkumar@ssdiblr.sharp.co.in>
2025-06-11 15:58:44 +0530

    frontEnd

---
+++ notesapp/src/app/create/create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-create',
+  imports: [RouterLink],
+  templateUrl: './create.component.html',
+  styleUrl: './create.component.css'
+})
+export class CreateComponent {
+
+}
