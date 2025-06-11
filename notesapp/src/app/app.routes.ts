36a87f20d6dd8e6ce05f055e58ddafc0a5fc1543
Dineshkumar Arunachalam <dineshkumar@ssdiblr.sharp.co.in>
2025-06-11 19:44:10 +0530

    updated UI

--- notesapp/src/app/app.routes.ts
+++ notesapp/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { CardComponent } from './card/card.component';
-import { createComponent } from '@angular/core';
 import { EditComponent } from './edit/edit.component';
 import { CreateComponent } from './create/create.component';
 
@@ -9,12 +8,12 @@ export const routes: Routes = [
         path:"",redirectTo:"card", pathMatch:"full", title:"Work Item-Think Board"
     },
     {
-        path:"card",component:CardComponent, title:"Work Item-Think Board"
+        path:"card",component:CardComponent, title:"Work Item - Think Board"
     },
     {
-        path:"create",component:CreateComponent, title:"Create Item-Think Board"
+        path:"create",component:CreateComponent, title:"Create Item -Think Board"
     },
     {
-        path:"edit",component:EditComponent, title:"Edit Item-Think Board"
+        path:"edit",component:EditComponent, title:"Edit Item - Think Board"
     }
 ];
